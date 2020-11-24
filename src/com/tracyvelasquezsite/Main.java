5cf96a9145ad7d31d755ee81d01626054be31a7d
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-24 17:22:51 -0600

    created find leagues method

--- src/com/tracyvelasquezsite/Main.java
+++ src/com/tracyvelasquezsite/Main.java
@@ -13,4 +13,27 @@ public class Main {
     }
 
 
+
+    public static List<League> findLeagues(String coords, int radius, int budget) {
+
+        List<League> leagues = new ArrayList<>();
+        leagues.add(new League("The Wyld Stallions", "1,1", 5, 4500));
+        leagues.add(new League("Team Zoidberg", "1,1", 5, 6000));
+        leagues.add(new League("The Zoomers", "1,1", 5, 1500));
+        leagues.add(new League("North Horseburg Little League", "1,1", 5, 3500));
+        leagues.add(new League("The Duloc Ogres", "1,1", 5, 2500));
+        List<League> acceptedLeagues = new ArrayList<>();
+
+        for (League league : leagues) {
+            if (budget - league.getPrice() <= 0)
+                return acceptedLeagues;
+            else if (league.getPrice() <= budget) {
+                budget = budget - league.getPrice();
+                acceptedLeagues.add(league);
+            }
+        }
+        return acceptedLeagues;
+    }
+
+
 }
