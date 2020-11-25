8fef363cf03a160dbc931bcf4af4870ce3d47176
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-24 23:27:46 -0600

    finished functionality

--- src/com/tracyvelasquezsite/Main.java
+++ src/com/tracyvelasquezsite/Main.java
@@ -14,19 +14,25 @@ public class Main {
         leagues.add(new League("North Horseburg Little League", "1,1", 3500));
         leagues.add(new League("The Duloc Ogres", "1,1", 2500));
 
-        addLeague("The Losers", "1,1", 2200);
+        returnAllLeagues(); //returns list of original 5 Leagues ascending by price
 
-        sponsorLeagues("1,1", 5, 20000);
-        sponsorLeagues("1,1", 5, 1000);
         sponsorLeagues("1,1", 5, 8000);
+        // Should return The Zoomers, The Duloc Ogres, and The North Horseburg Little League
+
+        addLeague("The Losers", "1,1", 2200);
+        addLeague("The Boys", "1,1", 2200);
+        addLeague("The Mighty Ducks", "1,1", 2200);
+
+        returnAllLeagues(); //returns list of 8 leagues
+
+        sponsorLeagues("1,1", 5, 15000); //
+        sponsorLeagues("1,1", 5, 1000);
+        sponsorLeagues("1,1", 5, 10000);
     }
 
 
-    public static void addLeague(String name, String coords, int price){
+    public static void addLeague(String name, String coords, int price) {
         leagues.add(new League(name, coords, price));
-        for (League league : leagues) {
-            System.out.println(league.getName());
-        }
     }
 
     public static void sponsorLeagues(String coords, int radius, int budget) {
@@ -34,17 +40,19 @@ public class Main {
 
         int totalSpend = 0;
 
+        System.out.println("--------------------------------------");
         for (League acceptedLeague : acceptedLeagues) {
             if (budget - acceptedLeague.getPrice() <= 0) {
-                System.out.println("Your budget cannot sponsor a league.");
+                System.out.println("Your budget cannot be used to sponsor a league.");
             }
             System.out.println("Name:" + acceptedLeague.getName());
-            System.out.println("Price:" + acceptedLeague.getPrice());
+            System.out.println("Price: $" + acceptedLeague.getPrice());
             totalSpend = totalSpend + acceptedLeague.getPrice();
         }
+        System.out.println("Total Budget: $" + budget);
+        System.out.println("Total Budget Used: $" + totalSpend);
+        System.out.println("Total Budget Remaining: $" + (budget - totalSpend));
         System.out.println("--------------------------------------");
-        System.out.println("Total Budget Used: " + totalSpend);
-        System.out.println("Total Budget Remaining: " + (budget - totalSpend));
     }
 
     public static List<League> findLeagues(String coords, int radius, int budget) {
@@ -61,6 +69,15 @@ public class Main {
             }
         }
         return acceptedLeagues;
+    }
+
+    public static void returnAllLeagues() {
+        Collections.sort(leagues, Comparator.comparingInt(League::getPrice));
+        System.out.println("Here is the full list of leagues: ");
+        for (int i = 0; i < leagues.size(); i++) {
+            System.out.println("Name: " + leagues.get(i).getName());
+            System.out.println("Price: $" + leagues.get(i).getPrice());
+        }
     }
 
 }
